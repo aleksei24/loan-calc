80498262f676df04ec291b096702bb76c2343dc6
aleksei24 <lexa24032@gmail.com>
2022-01-03 20:18:49 +0300

    design is being made

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// styles
+import 'tailwindcss/base.css';
+import 'tailwindcss/components.css';
+import 'tailwindcss/utilities.css';
+
+// scripts
+import './script/view';
---
+++ src/script/view.js
@@ -0,0 +1,9 @@
+const app = document.querySelector('#app');
+app.classList =
+  'container mx-auto px-2 py-16 max-w-lg h-full min-h-screen border-2 border-solid border-gray-500 rounded-md';
+
+const appHeadline = document.querySelector('#app-headline');
+appHeadline.classList = 'text-center font-bold uppercase';
+
+const loanForm = document.querySelector('#loan-form');
+loanForm.classList = 'flex flex-col items-end';
