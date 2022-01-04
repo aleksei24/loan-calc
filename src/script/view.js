50ede4fee15cff9f57082d40cfc4b8c79d39025a
aleksei24 <lexa24032@gmail.com>
2022-01-04 09:40:11 +0300

    making design

--- src/script/view.js
+++ src/script/view.js
@@ -1,9 +1,23 @@
 const app = document.querySelector('#app');
 app.classList =
-  'container mx-auto px-2 py-16 max-w-lg h-full min-h-screen border-2 border-solid border-gray-500 rounded-md';
+  'container mx-auto px-2 py-16 max-w-lg h-full min-h-screen min-w-fit border-2 border-solid border-gray-500 rounded-md';
 
 const appHeadline = document.querySelector('#app-headline');
 appHeadline.classList = 'text-center font-bold uppercase';
 
 const loanForm = document.querySelector('#loan-form');
-loanForm.classList = 'flex flex-col items-end';
+loanForm.classList = 'flex flex-col items-center';
+
+const labels = loanForm.children;
+[...labels].forEach((el) => {
+  el.classList = 'my-3';
+});
+
+const amountInput = document.querySelector('#amount');
+const interestInput = document.querySelector('#interest');
+const paymentInput = document.querySelector('#payment');
+const loanInputs = [amountInput, interestInput, paymentInput];
+[...loanInputs].forEach((el) => {
+  el.classList = 'h-8 rounded';
+});
+// console.log(loanInputs);
